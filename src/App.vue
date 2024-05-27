43b42f4b8f97961c81ead57d395f70528226c50b
Talin Sharma <talinsharma.dev@gmail.com>
2024-05-27 14:35:44 -0400

    Game is basically done. TODO: Tutorial, better win display

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,200 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+import X from "./components/X.vue";
+import O from "./components/O.vue";
+
+let grid = ref([
+  [0, 0, 0],
+  [0, 0, 0],
+  [0, 0, 0],
+]);
+
+let turn = ref(1);
+let xScore = ref(0);
+let yScore = ref(0);
+
+function move(x, y) {
+  if (grid.value[x][y] !== 0) {
+    return;
+  }
+  if (turn.value === 1) {
+    grid.value[x][y] = 1;
+  } else {
+    grid.value[x][y] = 2;
+  }
+  winCheck();
+  turn.value = turn.value == 1 ? (turn.value = 2) : (turn.value = 1);
+}
+
+function winCheck() {
+  for (let [x, row] of grid.value.entries()) {
+    for (let [y, cell] of row.entries()) {
+      if (cell !== 0) {
+        if (row[y + 1] == cell && row[y - 1] == cell) {
+          // Horizontal
+          win();
+        } else if (x === 0) {
+          // Top Row
+          if (grid.value[x + 1][y] == cell && grid.value[x + 2][y] == cell) {
+            // Vertical
+            win();
+          } else if (y === 0) {
+            // First Cell
+            if (
+              grid.value[x + 1][y + 1] == cell &&
+              grid.value[x + 2][y + 2] == cell
+            ) {
+              // Diagonal Left
+              win();
+            }
+          } else if (y === 2) {
+            // Last Cell
+            if (
+              grid.value[x + 1][y - 1] == cell &&
+              grid.value[x + 2][y - 2] == cell
+            ) {
+              // Diagonal Right
+              win();
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+function win() {
+  setTimeout(() => {
+    reset();
+    if (turn.value == 1) {
+      xScore.value++;
+    } else {
+      yScore.value++;
+    }
+  }, 1000);
+}
+
+function reset() {
+  for (let x = 0; x < grid.value.length; x++) {
+    for (let y = 0; y < grid.value.length; y++) {
+      grid.value[x][y] = 0;
+    }
+  }
+  turn.value = 1;
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <h1>Tic-Tac-Toe</h1>
+  <div id="actionButtons">
+    <!-- <button id="tutorialButton">Tutorial</button> -->
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div id="game">
+    <div id="scoreCount">
+      <div id="xScore" class="score">{{ xScore }}</div>
+      <div id="oScore" class="score">{{ yScore }}</div>
+    </div>
+    <table id="grid">
+      <tr v-for="x in 3">
+        <td
+          v-for="y in 3"
+          :id="`${x - 1}.${y - 1}`"
+          @click="move(`${x - 1}`, `${y - 1}`)"
+        >
+          <X v-if="grid[x - 1][y - 1] === 1" class="x" />
+          <O v-if="grid[x - 1][y - 1] === 2" class="o" />
+        </td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+h1 {
+  font-family: "Bebas Neue", sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  font-size: clamp(28px, 3rem, 80px);
+  margin: 0px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+#actionButtons {
+  margin-bottom: 50px;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+button {
+  border: 0;
+  outline: none;
+  border-radius: 8px;
+  padding: 10px 20px;
+  font-size: clamp(12px, 0.5rem, 24px);
+  font-weight: 600;
+  background-color: var(--accent);
+  color: var(--text);
+
+  &:hover {
+    cursor: pointer;
+  }
+}
+
+table,
+tr,
+td {
+  border: 10px solid var(--accent);
+  border-collapse: collapse;
+}
+
+td {
+  --cell-dimensions: clamp(100px, 15vw, 300px);
+  --icon-dimensions: clamp(50px, 7vw, 200px);
+  width: var(--cell-dimensions);
+  height: var(--cell-dimensions);
+  text-align: center;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  .x {
+    width: var(--icon-dimensions);
+    height: var(--icon-dimensions);
+    color: var(--x);
+  }
+
+  .o {
+    width: var(--icon-dimensions);
+    height: var(--icon-dimensions);
+    color: var(--o);
+  }
+}
+
+#game {
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+
+  padding-right: 50px;
+
+  #scoreCount {
+    position: relative;
+    right: 50px;
+
+    .score {
+      margin: 10vh 0px;
+      font-size: 2rem;
+    }
+
+    #xScore {
+      color: var(--x);
+    }
+
+    #oScore {
+      color: var(--o);
+    }
+  }
+
+  #grid {
+    margin: auto;
+  }
 }
 </style>
---
+++ src/components/X.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg width="101" height="101" viewBox="0 0 101 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect x="0.109131" y="9.20004" width="12.8565" height="128.565" transform="rotate(-45 0.109131 9.20004)"
+    fill="currentColor" />
+    <rect x="91.0182" y="0.109131" width="12.8565" height="128.565" transform="rotate(45 91.0182 0.109131)"
+    fill="currentColor" />
+  </svg>
+</template>
